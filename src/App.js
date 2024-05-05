1ba403a3e877448faea6d5c2f4c6a25855911fed
KavinSPitt <kavin.sankar@gmail.com>
2024-05-05 12:27:23 -0700

    uploading project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import TextEditor from "./TextEditor"
+import TestingQuill from "./TestingQuill";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //return <TextEditor></TextEditor>;
+  return <TestingQuill></TestingQuill>;
 }
 
 export default App;
