797114bfc9e6ca7d3b6a45985022c74cf0625b8d
Kavin Sankar <90232501+KavinSankar123@users.noreply.github.com>
2024-05-08 12:16:38 -0400

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import TextEditor from "./TextEditor"
+import TestingQuill from "./TestingQuill";
+
+function App() {
+  //return <TextEditor></TextEditor>; 
+  return <TestingQuill></TestingQuill>;
+}
+
+export default App;
